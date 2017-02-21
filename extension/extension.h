3c4b882a3105c0ac7be290d60b12ec9830af4f65
JRiipinen <Pelipoika@users.noreply.github.com>
2017-02-21 22:26:50 +0200

    Revert "Fixed bot squad"

--- extension/extension.h
+++ extension/extension.h
@@ -1,9 +1,9 @@
 #ifndef _INCLUDE_REALIZESPYFIXER_H_
 	#define _INCLUDE_REALIZESPYFIXER_H_
-#include "smsdk_ext.h"
-#include "CDetour/detours.h"
 
-class CBotControl : public SDKExtension
+#include "smsdk_ext.h"
+
+class CRealizeSpyFixer : public SDKExtension
 {
 	public:
 		virtual bool SDK_OnLoad(char *error, size_t maxlength, bool late);
